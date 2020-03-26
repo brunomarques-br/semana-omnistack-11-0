274135d6ccff6ecf0295472fb783621527fd606e
Bruno Marques <brunomandrade.br@gmail.com>
2020-03-26 10:22:49 -0300

    Backend finalizado, rotas e funcionalidades do app criadas.

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,25 @@ const connection = require('../database/connection');
 module.exports = {
 
     async index(request, response) {
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select([
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
+
+        response.header('X-Total-Count', count['count(*)']);
+
         return response.json(incidents);
     },
 
@@ -14,10 +32,30 @@ module.exports = {
         const [id] = await connection('incidents').insert({
             title,
             description,
-            value, 
+            value,
             ong_id
         })
 
         return response.json({ id });
-    }
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+        // TO-DO (bugfix pending..)
+        if (incident.ong_id != ong_id) {
+            return response.status(401).json({ error: 'Operation not permitted.' });
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
+
+    },
 };
