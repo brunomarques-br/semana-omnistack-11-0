10360248d91af26266d29e4c9b971a2638fb7fa8
Bruno Marques <brunomandrade.br@gmail.com>
2020-03-26 01:44:41 -0300

    Criaçao das tabelas 'ongs' e 'incidents', rotas criadas 'index, create' e estrutura basica definida

---
+++ backend/readme.md
@@ -0,0 +1,20 @@
+<h1> Semana OmniStack 11.0 </h1>
+<h2> Sistema ONG - Backend </h2>
+<h2> Projeto desenvolvido utilizando as seguintes tecnologias: </h2>
+
+* Node.js ~ v13.9.0
+* Express.js ~ "^4.17.1"
+* NPM ~ 6.14.3
+* Sqlite3 ~ "^4.1.1" + Knex "^0.20.13"
+
+
+<h2> Funcionalidades </h2>
+
+* Login / Logout de ONG
+* Cadastro de ONG
+* Cadastrar novos casos
+* Deletar casos
+* Listar casos espec[ificos de uma ONG
+* Listar todos os casos
+* Entrar em contato com a ONG
+
---
+++ backend/src/database/migrations/20200325153811_create_ongs.js
@@ -0,0 +1,17 @@
+
+// Método de criação da tabela.
+exports.up = function(knex) {
+  return knex.schema.createTable('ongs', function (table){
+    table.string('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('whatsapp').notNullable();
+    table.string('city').notNullable();
+    table.string('uf', 2).notNullable();
+  });
+};
+
+// Método preventico, caso precise voltar a um estado da tabela
+exports.down = function(knex) {
+  return knex.schema.dropTable('ongs');
+};
