3b88b67e44545054334b6daacdc0bcd48c43460d
Bruno Marques <brunomandrade.br@gmail.com>
2020-03-27 01:29:55 -0300

    Definido a estrutura do projeto, criação das rotas e navegação do frontend (Logon e Register)

---
+++ frontend/readme.md
@@ -0,0 +1,18 @@
+<h1> Semana OmniStack 11.0 </h1>
+<h2> Sistema ONG - Frontend (React) </h2>
+<h2> Projeto desenvolvido utilizando as seguintes tecnologias: </h2>
+
+* React / React Dom ~ ^16.13.1
+* React Icons ~ ^3.9.0
+* React Router Dom ~ ^5.1.2
+
+<h2> Funcionalidades </h2>
+
+* Login / Logout de ONG
+* Cadastro de ONG
+* Cadastrar novos casos
+* Deletar casos
+* Listar casos específicos de uma ONG
+* Listar todos os casos
+* Entrar em contato com a ONG
+
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css';
+
+export default function Register() {
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero" className="src" />
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+                    <Link to="/" className="back-link">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Voltar para o Logon
+                    </Link>
+
+                </section>
+                <form>
+                    <input placeholder="Nome da ONG" />
+                    <input type="email" placeholder="E-mail" />
+                    <input placeholder="Whatsapp" />
+
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" style={{ width: 80 }} />
+                    </div>
+
+                    <button className="button">Cadastrar</button>
+
+                </form>
+            </div>
+        </div>
+    );
+}
